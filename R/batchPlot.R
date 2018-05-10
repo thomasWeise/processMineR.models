73e459da00226535c48fbff0c91123bcb5d4c885
Thomas Weise <tweise@hfuu.edu.cn>
2018-05-10 13:44:04 +0800

    Now Inheriting Parameters from batchPlot.list

--- R/batchPlot.R
+++ R/batchPlot.R
@@ -1,22 +1,20 @@
 #' @title Plot all the Curves from a List of Models
 #' @description A simple utility method for visualizing the data.
 #' @param models an instance or list of \code{\link{Models}}
-#' @param log the logarithmic scaling info for the axes
-#' @param plotPoints should the points be plotted?
-#' @param plotFun should the fitted function models be plotted?
-#' @param printNames should the model names be printed as legend
-#' @importFrom plotteR batchPlot.groups
+#' @param names the model names, or \code{NULL} if they should not be printed
+#' @importFrom plotteR batchPlot.groups batchPlot.list
 #' @importClassesFrom regressoR RegressionResult
+#' @inheritDotParams plotteR::batchPlot.list -data -names -xfun -yfun -ffun
 #' @include Models.R
 #' @export batchPlot.Models
-batchPlot.Models <- function(models, log="", plotPoints=FALSE, plotFun=TRUE, printNames=(length(models)<10)) {
+batchPlot.Models <- function(models,
+                             names=if(length(models)<10) Models.names(models) else NULL,
+                             ...) {
   batchPlot.groups(data=models,
                    extract=function(model) model@models,
                    xfun=function(result) result@metric@x,
                    yfun=function(result) result@metric@y,
                    ffun=function(result, x) result@result@f(x),
-                   plotXY=plotPoints,
-                   plotXF=plotFun,
-                   log=log,
-                   names=if(printNames) Models.names(models) else NULL)
+                   names=names,
+                   ...)
 }
