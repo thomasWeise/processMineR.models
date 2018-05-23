b11441dc23fe5cfe0b44d5f971c2c26e342896c6
Thomas Weise <tweise@hfuu.edu.cn>
2018-05-24 07:59:08 +0800

    Including Monotonous Functional Models back into the Learning Process

--- R/batchLearn.R
+++ R/batchLearn.R
@@ -1,25 +1,35 @@
-# create the default models
+# create the default models for low fitting effort
 #' @importFrom regressoR.splines regressoR.spline.protected
-.default <- regressoR.spline.protected();
-.default <- force(.default);
+.defaultA <- regressoR.spline.protected();
+.defaultA <- force(.defaultA);
+
+# create the default models for high fitting effort
+#' @importFrom regressoR.functional FunctionalModel.monotonousLearners
+.defaultB <- unlist(c(.defaultA, FunctionalModel.monotonousLearners()),
+                    recursive=TRUE);
+.defaultB <- force(.defaultB);
+
 
 #' @title Batch-Learn Regression Models for Process Mining
 #' @description This is a wrapper around
 #'   \code{\link[regressoR]{regressoR.batchLearn}} which should provide a
 #'   shortcut to the recommended modeling techniques.
-#' @param learners the model learners to be applied
+#' @param q the modeling effort to be applied
+#' @param learners the model learners to be applied, which has direct
+#'   implications on the runtime, fitting quality, and models tested
 #' @param cores the number of cores to use (\code{>1L} leads to parallel
 #'   execution)
 #' @export Models.batchLearn
 #' @importFrom parallel detectCores
 #' @importFrom regressoR regressoR.batchLearn
-#' @inheritDotParams regressoR::regressoR.batchLearn -learners -cores
-Models.batchLearn <- function(learners=.default,
+#' @inheritDotParams regressoR::regressoR.batchLearn -learners -cores -q
+Models.batchLearn <- function(q=0.2,
+                              learners=if(q > 0.15) .defaultB else .defaultA,
                               cores=detectCores(),
                               ...) {
   pars <- list(...);
   pars$learners <- learners;
   pars$cores <- cores;
-  if(is.null(pars$q)) { pars$q <- 0.05; }
+  pars$q <- q;
   return(do.call(regressoR.batchLearn, pars));
 }
